894be1ac26592a368d09b3c8e8db88d37ec15d75
Paul Sajna <hello@paulsajna.com>
2022-05-23 21:00:31 -0700

    if let

--- src/main.rs
+++ src/main.rs
@@ -18,42 +18,30 @@ fn main() {
 }
 
 fn file_has_syms(path: &Path) -> bool {
-    let tmp = fs::read(path);
-    let bytes: Vec<u8>;
-    if tmp.is_ok() { bytes = tmp.unwrap(); } else { return false; }
-    let header = Elf::parse_header(&bytes).ok();
-    match header {
-        Some(header) => {
+    let bytes = fs::read(path);
+    if let Ok(bytes) = bytes {
+        let header = Elf::parse_header(&bytes);
+        if let Ok(header) = header {
             let elf = Elf::lazy_parse(header).ok();
-            match elf {
-                Some(mut elf) => {
-                    let ctx = container::Ctx {
-                        container: container::Container::Little,
-                        le: container::Endian::Little,
-                    };
-                    let sh = SectionHeader::parse(&bytes, header.e_shoff as usize, header.e_shnum as usize, ctx).ok();
-                    match sh {
-                        Some(section_headers) => {
-                            let strtab_idx = header.e_shstrndx as usize;
-                            let tmp = get_strtab(&bytes, &section_headers, strtab_idx);
-                            if tmp.is_ok() {
-                                elf.shdr_strtab = tmp.unwrap();
-                            } 
-                            else {
-                                return false;
-                            }
-
+            if let Some(mut elf) = elf {
+                let ctx = container::Ctx {
+                    container: container::Container::Little,
+                    le: container::Endian::Little,
+                };
+                let sh = SectionHeader::parse(&bytes, header.e_shoff as usize, header.e_shnum as usize, ctx).ok();
+                if let Some(section_headers) = sh {
+                    let strtab_idx = header.e_shstrndx as usize;
+                    let shdr_strtab = get_strtab(&bytes, &section_headers, strtab_idx);
+                    if let Ok(shdr_strtab) = shdr_strtab {
+                            elf.shdr_strtab = shdr_strtab;
                             elf.section_headers = section_headers;
                             return section_headers_indicate_syms(elf);
-                        },
-                        _ => return false,
-                    }
+                    };
                 }
-                None => return false,
             }
-        },
-        None => return false,
+        }
     }
+    false
 }
 
 fn section_headers_indicate_syms(elf: goblin::elf::Elf) -> bool {
@@ -61,13 +49,16 @@ fn section_headers_indicate_syms(elf: goblin::elf::Elf) -> bool {
     let mut good_strtab = false;
     for header in elf.section_headers.into_iter() {
         let sym_opt = elf.shdr_strtab.get_at(header.sh_name);
-        if sym_opt.is_some() && sym_opt.unwrap() == ".symtab" && header.sh_size > 16 {
-            good_symtab = true;
-        } else if sym_opt.is_some() && sym_opt.unwrap() == ".strtab" && header.sh_size > 16 {
-            good_strtab = true;
+        if let Some(".symtab") = sym_opt {
+            if header.sh_size > 16 {
+                good_symtab = true;
+            }
+        } else if let Some(".strtab") = sym_opt {
+            if  header.sh_size > 16 {
+                good_strtab = true;
+            }
         }
     }
-    
     good_symtab && good_strtab
 }
 
